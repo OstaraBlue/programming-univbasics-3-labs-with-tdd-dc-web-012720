2b1e761079566a593cf141365e4fad8723166d67
Sonata Arrington <sarrington1009@gmail.com>
2020-01-09 00:19:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
 first_number = 14 # Add your variables here
 second_number = 8
 sum = first_number + second_number
-differences = first_number - second_number
+difference = first_number - second_number
